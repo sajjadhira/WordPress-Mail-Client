163589acd465b07e373c5fa85b9b6d2db7f8eea6
sajjadhira <sajjad.hira12@gmail.com>
2023-09-22 23:43:26 +0600

    initial push

---
+++ mailclient.php
@@ -0,0 +1,18 @@
+<?php
+/*
+Plugin Name: Mail Client
+Plugin URI: https://pluginoo.com
+Description: A simple mail connection plugin
+Version: 1.0
+Author: Pluginoo
+Author URI: https://pluginoo.com
+License: GPLv2 or later
+Text Domain: mailclient
+*/
+class MailClient
+{
+    public function __construct()
+    {
+        // init plugin
+    }
+}
