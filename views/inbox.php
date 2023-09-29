9b5932245e1df2284388ef48bd4ad311d839d8f0
sajjadhira <sajjad.hira12@gmail.com>
2023-09-29 23:47:52 +0600

    list design added

--- views/inbox.php
+++ views/inbox.php
@@ -4,4 +4,20 @@
     <div class="flex flex-row w-full h-16 bg-white-600">
         <h2 class="font-bold text-3xl p-3">Inbox</h2>
     </div>
+
+
+    <!-- create full page card with tailwind css -->
+
+    <div class="bg-white font-bold p-3 text-lg mb-1 rounded">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab, fugit?
+    </div>
+    <div class="bg-white font-bold p-3 text-lg mb-1 rounded">
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Consequatur quas at ipsam obcaecati autem dolores.
+    </div>
+    <div class="bg-white font-bold p-3 text-lg mb-1 rounded">
+        Lorem ipsum dolor sit, amet consectetur adipisicing.
+    </div>
+    <div class="bg-white p-3 text-lg mb-1 rounded">
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quos, maxime cumque.
+    </div>
 </div>
