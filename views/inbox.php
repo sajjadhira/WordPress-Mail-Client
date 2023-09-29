fc43bf551f07e3ede5d83566b16e7348522d0594
sajjadhira <sajjad.hira12@gmail.com>
2023-09-29 23:41:08 +0600

    inbox page added

---
+++ views/inbox.php
@@ -0,0 +1,7 @@
+<!-- wirte list using tailwind css -->
+
+<div class="wrap">
+    <div class="flex flex-row w-full h-16 bg-white-600">
+        <h2 class="font-bold text-3xl p-3">Inbox</h2>
+    </div>
+</div>
