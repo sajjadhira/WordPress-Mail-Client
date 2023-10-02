eb6f47f8eb06a374ef414de540cac016b025b5ad
sajjadhira <sajjad.hira12@gmail.com>
2023-10-02 23:46:00 +0600

    settings view added

---
+++ views/settings.php
@@ -0,0 +1,9 @@
+<!-- wirte list using tailwind css -->
+
+<div class="wrap">
+    <div class="flex flex-row w-full h-16 bg-white-600">
+        <h2 class="font-bold text-3xl p-3">Settings</h2>
+    </div>
+
+    <!-- create view with tailwind css -->
+</div>
