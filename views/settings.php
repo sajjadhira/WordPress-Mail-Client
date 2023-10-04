1f5752d9b755d6cb4e7f8e423aa7528f2e018a73
sajjadhira <sajjad.hira12@gmail.com>
2023-10-04 22:43:57 +0600

    tailwind added to settings page

--- views/settings.php
+++ views/settings.php
@@ -1,50 +1,35 @@
-<!-- wirte list using tailwind css -->
-
 <div class="wrap">
-    <div class="flex flex-row w-full h-16 bg-white-600">
-        <h2 class="font-bold text-3xl p-3">Settings</h2>
+    <div class="flex flex-row w-full h-16 bg-gray-600 items-center">
+        <h2 class="font-bold text-3xl text-white pl-8">Settings</h2>
     </div>
 
-    <!-- create view with tailwind css -->
-
-    <form action="" method="post" class="bg-white p-3 text-lg mb-1 rounded">
-        <div class="flex flex-col">
-            <label for="email" class="text-lg font-bold">Email</label>
-            <input type="email" name="email" id="email" class="border-2 border-gray-400 rounded p-1">
+    <form action="" method="post" class="bg-white p-6 mt-3 mb-6 rounded-md shadow-md text-lg">
+        <div class="flex flex-col mb-4">
+            <label for="email" class="text-lg font-bold mb-2">Email</label>
+            <input type="email" name="email" id="email" class="border-2 border-gray-300 focus:border-blue-500 rounded-md p-2 w-full">
         </div>
-        <div class="flex flex-col">
-            <label for="password" class="text-lg font-bold">Password</label>
-            <input type="password" name="password" id="password" class="border-2 border-gray-400 rounded p-1">
+        <div class="flex flex-col mb-4">
+            <label for="password" class="text-lg font-bold mb-2">Password</label>
+            <input type="password" name="password" id="password" class="border-2 border-gray-300 focus:border-blue-500 rounded-md p-2 w-full">
         </div>
-        <div class="flex flex-col">
-            <label for="host" class="text-lg font-bold">Host</label>
-            <input type="text" name="host" id="host" class="border-2 border-gray-400 rounded p-1">
+        <div class="flex flex-col mb-4">
+            <label for="host" class="text-lg font-bold mb-2">Host</label>
+            <input type="text" name="host" id="host" class="border-2 border-gray-300 focus:border-blue-500 rounded-md p-2 w-full">
         </div>
-        <div class="flex flex-col">
-            <label for="port" class="text-lg font-bold">Port</label>
-            <input type="number" name="port" id="port" class="border-2 border-gray-400 rounded p-1">
+        <div class="flex flex-col mb-4">
+            <label for="port" class="text-lg font-bold mb-2">Port</label>
+            <input type="number" name="port" id="port" class="border-2 border-gray-300 focus:border-blue-500 rounded-md p-2 w-full">
         </div>
-        <div class="flex flex-col">
-            <label for="encryption" class="text-lg font-bold">Encryption</label>
-            <input type="text" name="encryption" id="encryption" class="border-2 border-gray-400 rounded p-1">
+        <div class="flex flex-col mb-4">
+            <label for="encryption" class="text-lg font-bold mb-2">Encryption</label>
+            <input type="text" name="encryption" id="encryption" class="border-2 border-gray-300 focus:border-blue-500 rounded-md p-2 w-full">
         </div>
-        <div class="flex flex-col">
-            <label for="name" class="text-lg font-bold">Name</label>
-            <input type="text" name="name" id="name" class="border-2 border-gray-400 rounded p-1">
+        <div class="flex flex-col mb-4">
+            <label for="name" class="text-lg font-bold mb-2">Name</label>
+            <input type="text" name="name" id="name" class="border-2 border-gray-300 focus:border-blue-500 rounded-md p-2 w-full">
         </div>
-        <div class="flex flex-col">
-            <label for="signature" class="text-lg font-bold">Signature</label>
-            <textarea name="signature" id="signature" cols="30" rows="10" class="border-2 border-gray-400 rounded p-1"></textarea>
-        </div>
-        <div class="flex flex-col">
-            <label for="signature" class="text-lg font-bold">Signature</label>
-            <textarea name="signature" id="signature" cols="30" rows="10" class="border-2 border-gray-400 rounded p-1"></textarea>
-
-        </div>
-
-        <!-- add submit button -->
-        <div class="flex flex-col">
-            <input type="submit" value="Save" class="bg-blue-500 text-white font-bold p-2 rounded">
+        <div class="flex flex-col mt-4">
+            <input type="submit" value="Save" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-md cursor-pointer">
         </div>
     </form>
 </div>
